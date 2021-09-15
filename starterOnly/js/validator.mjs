9069ca1cd72e47d4379d7461ce3e0ce3e7923bae
Alexandre Jung <jungalexandre@ymail.com>
2021-09-16 00:03:33 +0200

    :sparkles: feat: implement form inputs (#3)

---
+++ starterOnly/js/validator.mjs
@@ -0,0 +1,24 @@
+const nameRegex = /^[a-zA-Z]{2,}(?:-[a-zA-Z]{2,})?$/;
+const emailRegex = /^[a-z0-9]+([+._-][a-z0-9]+)*@[a-z0-9]+(-[a-z]+)*(\.[a-z]{2,})+$/;
+
+const Validator = {
+    name(name) {
+        return nameRegex.test(name);
+    },
+    email(email) {
+        return emailRegex.test(email);
+    },
+    birthdate(date) {
+        const today = new Date();
+        const testDate = new Date(date);
+        return testDate < today;
+    },
+    positiveInteger(value) {
+        const intValue = parseInt(value);
+        return value !== '' &&
+            value === String(intValue) &&
+            intValue >= 0;
+    },
+}
+
+export default Validator;
