c6ca6f98e3098462f3aa4261320c7b22b68c1f17
Alexandre Jung <jungalexandre@ymail.com>
2021-10-01 17:31:32 +0200

    :sparkles: feat: improve validation (#29)

--- starterOnly/static/js/formData.mjs
+++ starterOnly/static/js/formData.mjs
@@ -1,13 +1,15 @@
 import Mediator from "./mediator.mjs";
 
 class FormData extends Mediator.Component {
-    constructor(element) {
+    constructor(element, inputHandler = null) {
         super();
         this.element = element;
         this.input = element.querySelector('input');
         this.input.addEventListener('input', this.handleValueChange);
         this.input.addEventListener('focus', this.resetBackground);
         this.input.addEventListener('input', this.resetBackground);
+        this.timeout = null;
+        this.defaultMessage = this.element.dataset.error;
     }
     getValue() {
         return this.input.value;
@@ -49,6 +51,21 @@ class FormData extends Mediator.Component {
     handleValueChange = () => {
         this.notify('change', [this, this.getValue()]);
     }
+    showMessage(message, duration = 2000) {
+        clearTimeout(this.timeout);
+        this.element.dataset.error = message;
+        this.element.classList.add('message-only');
+        this.timeout = setTimeout(() => {
+            this.element.classList.remove('message-only');
+            setTimeout(() => {
+                this.element.dataset.error = this.defaultMessage;
+            }, 300);
+        }, duration);
+    }
+    resetMessage() {
+        this.element.classList.remove('message-only');
+        this.element.dataset.error = this.defaultMessage;
+    }
 }
 
 class Checkbox extends FormData {
@@ -65,9 +82,23 @@ class Checkbox extends FormData {
 }
 
 class NumberInput extends FormData {
+    constructor(element) {
+        super(element);
+        this.input.addEventListener('keydown', this.handleValueChange);
+    }
     clear() {
         this.input.value = 0;
         this.setState();
+    }
+    handleValueChange = ev => {
+        const backspace = ev.key === 'Backspace';
+        const enter = ev.key === 'Enter';
+        const digit = ev.key >= '0' && ev.key <= '9'
+        if (backspace || enter || digit) {
+            this.notify('change', [this, this.getValue()]);
+        } else {
+            ev.preventDefault();
+        }
     }
 }
 
