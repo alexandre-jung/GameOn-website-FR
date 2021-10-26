f615e98af919afb25e0f2c3ec5766ce9aa81c455
Alexandre Jung <jungalexandre@ymail.com>
2021-10-26 22:14:30 +0200

    :adhesive_bandage: fix(ui): close navbar when clicking outside

--- starterOnly/static/js/modal.mjs
+++ starterOnly/static/js/modal.mjs
@@ -1,9 +1,17 @@
 import Form from './form.mjs';
 
+const mainNavbar = document.querySelector(".main-navbar");
+mainNavbar.addEventListener("click", function(ev) {
+    ev.stopPropagation();
+});
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
+    document.addEventListener("click", function() {
+        x.className = "topnav";
+    });
   } else {
     x.className = "topnav";
   }
